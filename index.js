71ffc60357f7eced8e830d6d029aa68ae1655147
Jedel Ce <Cereceres@users.noreply.github.com>
2017-06-26 14:22:53 -0500

    Integrate method is added (#6)
    * the package json is added
    
    * derive method is added
    
    * integrate method is tested

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ const sdk =  class extends Core {
         
         return this.api('/derive/' + exp).then(res => res.result)
     }
+    
+    integrate(exp, a, b){
+        if(typeof exp === 'undefined') return Promise.reject(new Error('Undefined is not supported')) 
+
+        const path = typeof a !== 'undefined' && typeof b !== 'undefined' ? 
+        '/area/'+ a + ':' + b + '|' + exp : '/integrate/'+ exp
+        return this.api(path).then(res => res.result)
+    }
 }
 
 module.exports = new sdk()
